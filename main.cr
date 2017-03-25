7f2b9e596c879f208fa1741a1e19777686c7b526
Dhruv Rajvanshi <dhruv.rajvanshi@acetone.co>
2017-03-25 08:38:20 +0530

    Added _ pattern

--- main.cr
+++ main.cr
@@ -155,26 +155,31 @@ macro data(base, args)
     -> {
       %matcher_func = -> {
         \{% for lhs in cases.keys %}
-          \{% if lhs.class_name == "Path" %}
-            \{% 
-              lhs_class = lhs.names[0]
-             %}
+          \{% if lhs.class_name == "Underscore" %}
+              return \{{cases[lhs]}}
           \{% else %}
-            \{%
-               lhs_class = lhs[0].names[0]
-             %}
-          \{% end %}
-
-          if \{{val}}.is_a? {{base_class}}::\{{lhs_class}}
-            ## bind values
-            \{% if lhs.class_name != "Path" %}
-              \{% for i in 1...lhs.size %}
-                \{{lhs[i]}} = %value.as({{base_class}}::\{{lhs_class}}).value\{{i-1}}
-              \{% end %}
+            \{% if lhs.class_name == "Path" %}
+              \{% 
+                lhs_class = lhs.names[0]
+               %}
+            \{% else %}
+              \{%
+                 lhs_class = lhs[0].names[0]
+               %}
             \{% end %}
-            return \{{cases[lhs]}}
-          end
+
+            if \{{val}}.is_a? {{base_class}}::\{{lhs_class}}
+              ## bind values
+              \{% if lhs.class_name != "Path" %}
+                \{% for i in 1...lhs.size %}
+                  \{{lhs[i]}} = %value.as({{base_class}}::\{{lhs_class}}).value\{{i-1}}
+                \{% end %}
+              \{% end %}
+              return \{{cases[lhs]}}
+            end
+          \{% end %}
         \{% end %}
+        raise ArgumentError.new("Non exhaustive patterns passed to match_" + {{base_class.underscore.stringify}})
       }
       %matcher_func.call()
     }.call
@@ -267,8 +272,7 @@ data [List(A)], {
 # })
 
 puts match_list (List::Cons.new(true, List::Empty(Bool).new)), {
-  [Empty] => "Empty",
-  [Cons, x, xs] => x
+  [Cons, x, xs] => "Empty"
 }
 
 # puts match((List::Cons.new 1, List::Empty(Int32).new), {
@@ -322,3 +326,106 @@ puts match_list (List::Cons.new(true, List::Empty(Bool).new)), {
 # puts ap(f Some.new(1), Some.new(2), Some.new(3), Some.new(4)).to_s
 
 # print (a = 23)
+
+
+macro data_class(base, args)
+  {% if base[0].class_name == "Path" %}
+    {% base_class = base[0].names[0] %}
+  {% else %}
+    {% base_class = base[0].name.names[0] %}
+  {% end %}
+  # base class {{args[0]}}
+  macro match_{{base_class.underscore}}(val, cases)
+    %value = \{{val}}  
+    -> {
+      %matcher_func = -> {
+        \{% for lhs in cases.keys %}
+          \{% if lhs.class_name == "Underscore" %}
+              return \{{cases[lhs]}}
+          \{% else %}
+            \{% if lhs.class_name == "Path" %}
+              \{% 
+                lhs_class = lhs.names[0]
+               %}
+            \{% else %}
+              \{%
+                 lhs_class = lhs[0].names[0]
+               %}
+            \{% end %}
+
+            if \{{val}}.is_a? {{base_class}}::\{{lhs_class}}
+              ## bind values
+              \{% if lhs.class_name != "Path" %}
+                \{% for i in 1...lhs.size %}
+                  \{{lhs[i]}} = %value.as({{base_class}}::\{{lhs_class}}).value\{{i-1}}
+                \{% end %}
+              \{% end %}
+              return \{{cases[lhs]}}
+            end
+          \{% end %}
+        \{% end %}
+        raise ArgumentError.new("Non exhaustive patterns passed to match_" + {{base_class.underscore.stringify}})
+      }
+      %matcher_func.call()
+    }.call
+  end
+  class {{base[0]}}
+    {% if base[0].class_name == "Path" %}
+      # non generic base
+      {% for i in 0...args.size %}
+        {% if args[i].class_name == "Path" %}
+          class {{args[i].names[0]}} < {{base[0]}}
+            def initialize
+            end
+          end
+        {% else %}
+          class {{args[i].name}} < {{base[0]}}
+            {% for j in 0...args[i].type_vars.size %}
+              property value{{j}}
+            {% end %}
+            def initialize(
+              {% for j in 0...args[i].type_vars.size-1 %}
+                @value{{j}} : {{args[i].type_vars[j]}},
+              {% end %}
+              @value{{args[i].type_vars.size - 1}} : {{args[i].type_vars[args[i].type_vars.size - 1]}}
+            )
+            end
+          end
+        {% end %}
+      {% end %}
+    {% else %}
+      # generic base
+      {% for i in 0...args.size %}
+        {% if args[i].class_name == "Path" %} # constructor with no value types
+          class {{args[i].names[0]}}(
+              {{base[0].type_vars[0]}}
+              {% for j in 1...base[0].type_vars.size %}
+                , {{base[0].type_vars[j]}}
+              {% end %}
+            ) < {{base[0]}}
+            def initialize
+            end
+          end
+        {% else %} # intersection type
+          class {{args[i].name}}(
+              {{base[0].type_vars[0]}}
+              {% for j in 1...base[0].type_vars.size %}
+                , {{base[0].type_vars[j]}}
+              {% end %}
+            ) < {{base[0]}}
+            {% for j in 0...args[i].type_vars.size %}
+              property value{{j}}
+            {% end %}
+            def initialize(
+              {% for j in 0...args[i].type_vars.size-1 %}
+                @value{{j}} : {{args[i].type_vars[j]}},
+              {% end %}
+              @value{{args[i].type_vars.size - 1}} : {{args[i].type_vars[args[i].type_vars.size - 1]}}
+            )
+            end
+          end
+        {% end %}
+      {% end %}
+    {% end %}
+  end
+end
